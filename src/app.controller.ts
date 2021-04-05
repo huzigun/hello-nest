b510b59314249d1f24c4ab61802ea7291bff44aa
huzigun <ungyul@gmail.com>
2021-04-06 01:18:38 +0900

    nest init & passport init

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Req() req) {
+    return req.user;
+  }
+}
